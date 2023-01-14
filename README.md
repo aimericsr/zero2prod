6da7e99ffb8cc9733ec8320bd2d96929b170fa21
aimericsr <52284219+aimericsr@users.noreply.github.com>
2023-01-14 22:34:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+cargo install cargo-watch
+cargo install cargo-audit
+cargo install cargo-edit
+cargo install cargo-expand
+cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres
+cargo install cargo-udeps
+cargo +nightly udeps
+cargo add actix-web@4.0.0
+cargo add tokio@1.22.0 (tokio = { version = "1.22.0", features = ["macros", "rt-multi-thread"] })
+
+cargo test
+cargo tarpaulin --ignore-tests
+cargo clippy -- -D warnings
+cargo fmt -- --check
+cargo audit
+
+cargo watch -x check -x test -x run
+rustup toolchain install nightly --allow-downgrade
+cargo +nightly expand
+
+
+sqlx migrate add mig1
+SKIP_DOCKER=true ./scripts/init_db.sh
+
+run certain tests : 
+TEST_LOG=true cargo test health_check_works
+
+docker build --tag zero2prod .
+docker run -d --name rust-api -p 8000:8000 zero2prod
+
+cargo sqlx prepare -- --lib
+
+cargo build --tests
+
+deploy(digital ocean) : 
+brew install doctl
+create read and write token (dop_v1_6862d8dae5aa83704f98d9f07d9cb89b232e4c96971f4283af389ac0a6d6cdc9)
+doctl auth init
+doctl apps create --spec spec.yaml
---
+++ src/startup.rs
@@ -0,0 +1,90 @@
+use actix_web::{
+    dev::Server,
+    web::{self, Data},
+    App, HttpServer,
+};
+use sqlx::{postgres::PgPoolOptions, PgPool};
+use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
+
+use crate::{
+    configuration::{DatabaseSettings, Settings},
+    email_client::EmailClient,
+    routes::{health_check, subscribe},
+};
+
+// A new type to hold the newly built server and its port
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+        let connection_pool = get_connection_pool(&configuration.database);
+
+        let sender_email = configuration
+            .email_client
+            .sender()
+            .expect("Invalid sender email address.");
+
+        let timeout = configuration.email_client.timeout();
+        let email_client = EmailClient::new(
+            configuration.email_client.base_url,
+            sender_email,
+            configuration.email_client.authorization_token,
+            timeout,
+        );
+
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+        let listener = TcpListener::bind(address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = run(listener, connection_pool, email_client)?;
+        // We "save" the bound port in one of `Application`'s fields
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+    // A more expressive name that makes it clear that
+    // this function only returns when the application is stopped.
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.with_db())
+}
+
+// Notice the different signature!
+// We return `Server` on the happy path and we dropped the `async` keyword // We have no .await call, so it is not needed anymore.
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let db_pool = Data::new(db_pool);
+    let email_client = Data::new(email_client);
+
+    let server = HttpServer::new(move || {
+        App::new()
+            // Middlewares are added using the `wrap` method on `App`
+            .wrap(TracingLogger::default())
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
+            .app_data(email_client.clone())
+    })
+    .listen(listener)?
+    .run();
+    // No .await here!
+    Ok(server)
+}
